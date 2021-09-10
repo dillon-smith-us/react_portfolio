2687f9b2cd730e1f96b98d8d288b4e2815da32e6
Dillon Smith <dillsmth@gmail.com>
2021-09-09 21:51:43 -0500

    you are awesome

---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Jumbo from '../components/Jumbo'
+import Carousel from '../components/Carousel'
+
+function HomePage(props) {
+    return(
+        <div>
+        <Jumbo title={props.title} subTitle={props.subTitle} text={props.text}/>
+        <Carousel />
+    </div>
+    )
+}
+
+export default HomePage;
